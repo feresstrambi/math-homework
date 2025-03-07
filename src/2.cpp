0316678868995e6bf75d75910906e7673b7cd56c
feresstrambi <feresstrambi@gmail.com>
2025-03-07 21:51:53 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  // Homework problem 1
+  int a = 3;
+  int b = 5;
+  int c = a + b;
+  cout << "a + b = " << c << endl;
+  
+  // Homework problem 2
+  double d = 3.14;
+  double e = 2.71;
+  double f = d * e;
+  cout << "d * e = " << f << endl;
+  
+  // Homework problem 3
+  int g = 5;
+  int h = 8;
+  int i = g / h;
+  cout << "g / h = " << i << endl;
+  
+  // Homework problem 4
+  double j = 2.0;
+  double k = 3.0;
+  double l = j + k;
+  cout << "j + k = " << l << endl;
+  
+  return 0;
+}
