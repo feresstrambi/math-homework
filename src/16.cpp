32c50b453ee073129ec02c81a4973884e4391b50
feresstrambi <feresstrambi@gmail.com>
2025-03-21 04:15:15 +0000

    Add 16.cpp

---
+++ src/16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1 = 5;
+    int number2 = 3;
+    
+    if (number1 > number2) {
+        cout << "Number1 is greater than Number2." << endl;
+    } else {
+        cout << "Number1 is not greater than Number2." << endl;
+    }
+    
+    return 0;
+}
