3d5663dd76e7975cd2104d3c12f0bf665f393ebd
feresstrambi <feresstrambi@gmail.com>
2025-04-07 04:49:31 +0000

    Add 28.cpp

---
+++ src/28.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.141592653589793238;
+
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+
+    return 0;
+}
