07b0e18713d0fac5814dfe45d11f27dc802ff75d
feresstrambi <feresstrambi@gmail.com>
2025-04-08 14:58:02 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    double c = (a + b) / 2.0;
+    cout << "The result is: " << c << endl;
+    return 0;
+}
