7cf6f12127f3865045272e8a957035bf6729acfa
feresstrambi <feresstrambi@gmail.com>
2025-04-25 00:04:47 +0000

    Add 35.cpp

---
+++ src/35.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number" << endl;
+    } else if (x == 0) {
+        cout << "Zero" << endl;
+    } else {
+        cout << "Negative number" << endl;
+    }
+    return 0;
+}
