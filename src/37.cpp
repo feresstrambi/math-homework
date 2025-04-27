276e91cf538868e9f498cbe1e133666d579399f9
feresstrambi <feresstrambi@gmail.com>
2025-04-27 14:31:23 +0000

    Add 37.cpp

---
+++ src/37.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    if (num1 > num2) {
+        cout << "The first number is larger." << endl;
+    } else {
+        cout << "The second number is larger." << endl;
+    }
+
+    return 0;
+}
